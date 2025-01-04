25636cd2027c2eafc710ca732090d905f983e883
Cuervo-Team-Supreme <145875276+Nimodo83@users.noreply.github.com>
2025-01-04 00:52:22 -0600

    Update main.js

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ USO NO PRIVADO ES PUBLICO
 PERO CUERVO SE ENCARGA 
 */
 process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '1'
-import './settings.js'
+import './config.js'
 import {createRequire} from 'module'
 import path, {join} from 'path'
 import {fileURLToPath, pathToFileURL} from 'url'
